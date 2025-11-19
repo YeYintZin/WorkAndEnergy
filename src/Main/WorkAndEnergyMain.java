c2c2cecf3467faee3bad344741450a57f74673bd
Ye Yint Zin <6289490@edu.vaniercollege.qc.ca>
2025-11-19 14:27:05 -0500

    Initial Project

---
+++ src/Main/WorkAndEnergyMain.java
@@ -0,0 +1,24 @@
+package Main;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WorkAndEnergyMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/welcome.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Ski Simulator - Work and Energy");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
