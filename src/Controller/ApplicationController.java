85072f396ca35893331f4f4b9ce4bd0db67ede98
Ye Yint Zin <6289490@edu.vaniercollege.qc.ca>
2025-11-19 15:42:41 -0500

    Added second window, start and reset, and countdown Label

--- src/Controller/ApplicationController.java
+++ src/Controller/ApplicationController.java
@@ -1,14 +1,36 @@
 package Controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class ApplicationController implements Initializable {
+public class ApplicationController {
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    private Button reset;
+    @FXML
+    private Button start;
+    @FXML
+    private Label countdown;
+    
+    public void initialize() {
+        start.setOnAction(e -> {
+            if (start.getText().equals("Start")) {
+                start.setText("Stop");
+                reset.setDisable(false);
+            } else {
+                start.setText("Start");
+                reset.setDisable(true);
+            }
+            
+        });
+        reset.setOnAction(e -> {
+            resetPressed();
+        });
+    }
+    
+    private void resetPressed() {
+        System.out.println("reset");
+    }
     
 }
