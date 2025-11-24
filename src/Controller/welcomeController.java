c7d73bd617f81f1aeed55e8b6296ab0112b4e037
AlexLe06 <tanalexle06@gmail.com>
2025-11-24 08:55:12 -0500

    Modified welcomeFXML and added switchScene to welcomeController

--- src/Controller/welcomeController.java
+++ src/Controller/welcomeController.java
@@ -1,12 +1,19 @@
 package Controller;
 
+import java.io.IOException;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class welcomeController {
-
+    private Stage stage;
+    private Scene scene;
+    
     @FXML
     private Button creditsButton;
 
@@ -28,9 +35,31 @@ public class welcomeController {
 
     @FXML
     void playButtonAction(ActionEvent event) {
-
+        switchToApplication();
     }
+    
     public void initialize() {
         // TODO
     }    
+    
+    public void switchToApplication() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/application.fxml"));
+            Parent root = loader.load();
+
+            ApplicationController controller = loader.getController();
+            
+//            controller.setRunners(runners);
+            
+            stage = (Stage) playButton.getScene().getWindow();
+            scene = new Scene(root);
+            stage.setTitle("Marathon Simulator");
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
 }
